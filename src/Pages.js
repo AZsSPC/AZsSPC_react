cd0360d6a10e141a3c9b6658db2cbb25dbfc2db4
AZsSPC <ratmir.miro@gmail.com>
2026-02-08 00:53:45 +0200

    and now it is time for adding projects...

--- src/Pages.js
+++ src/Pages.js
@@ -19,42 +19,6 @@ const PROJECTS = {
         tags: ['test', 'demo'],
         marks: ['wip']
     },
-    '/projects/test1': {
-        path: 'projects/test',
-        title: 'Test project',
-        description: 'This is a test project used to demonstrate the project page layout. It has no actual content.',
-        tags: ['test', 'demo']
-    },
-    '/projects/test2': {
-        path: 'projects/test',
-        title: 'Test project',
-        description: 'This is a test project used to demonstrate the project page layout. It has no actual content.',
-        tags: ['test', 'demo']
-    },
-    '/projects/tes3t': {
-        path: 'projects/test',
-        title: 'Test project',
-        description: 'This is a test project used to demonstrate the project page layout. It has no actual content.',
-        tags: ['test', 'demo']
-    },
-    '/projects/test4': {
-        path: 'projects/test',
-        title: 'Test project',
-        description: 'This is a test project used to demonstrate the project page layout. It has no actual content.',
-        tags: ['test', 'demo']
-    },
-    '/projects/test5': {
-        path: 'projects/test',
-        title: 'Test project',
-        description: 'This is a test project used to demonstrate the project page layout. It has no actual content.',
-        tags: ['test', 'demo']
-    },
-    '/projects/test6': {
-        path: 'projects/test',
-        title: 'Test project',
-        description: 'This is a test project used to demonstrate the project page layout. It has no actual content.',
-        tags: ['test', 'demo']
-    },
 }
 
 const ALL_PAGES = {
