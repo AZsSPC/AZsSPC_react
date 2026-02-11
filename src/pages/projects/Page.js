a160a6fdf64619ac2302e8426528302d058f8089
AZsSPC <ratmir.miro@gmail.com>
2026-02-11 12:09:00 +0200

    little fix, now you can open projects from list page

--- src/pages/projects/Page.js
+++ src/pages/projects/Page.js
@@ -7,7 +7,7 @@ function Page() {
     return <>
         <div id='projects'>
             {Object.keys(PROJECTS).map(project => (
-                <AZLinkPanel key={project} href={`/${PROJECTS[project].path}/`} classes='project-link-panel'>
+                <AZLinkPanel key={project} href={project} classes='project-link-panel'>
                     <span className='project-path'>AZsSPC{project}</span>
                     <span className='project-title'>{PROJECTS[project].title || project}</span>
                     <span className='project-description'>{PROJECTS[project].description}</span>
